51592cfe62c531d499e94336d5a26ba7258facba
arsk78 <shotas777888@outlook.jp>
2019-01-19 16:01:29 +0900

    プロフィール登録

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root 'top#index'
 
-  resources :users, only: %i(show)
+  resources :users, only: %i(index show)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
